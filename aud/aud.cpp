00b6caf84d8fdf30a0c3ec098e5037b668595318
SkyLight <35933068+SkyLight2000@users.noreply.github.com>
2018-02-20 18:52:24 +0200

    Update aud.cpp

--- aud/aud.cpp
+++ aud/aud.cpp
@@ -5,29 +5,26 @@
 #include <iostream>
 #include <Windows.h>
 using namespace std;
- 
+
 int main()
 {
 	SetConsoleOutputCP(1251);
 	int arr[5];
-	printf("Введите числа: ");
+	printf("Р’РІРµРґРёС‚Рµ С‡РёСЃР»Р°: ");
 	for (int i = 0; i < 5; i++) {
 		cin >> arr[i];
 	}
 	int MAX = arr[0];
+	int MIN = arr[1];
 	for (int i = 0; i < 5; i++) {
 		if (MAX < arr[i]) {
 			MAX = arr[i];
 		}
-	}
-	int MIN = arr[1];
-	for (int i = 0; i < 5; i++) {
 		if (MIN > arr[i]) {
 			MIN = arr[i];
 		}
 	}
 	printf("MAX is %d, MIN is %d\n", MAX, MIN);
 	system("pause");
-    return 0;
+	return 0;
 }
-
